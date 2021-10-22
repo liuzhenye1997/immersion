6b8adae1603db66c2db3d2d1b3a5ad6164b63e7c
liuzhenye1997 <lzy1997@mail.ustc.edu.cn>
2021-10-22 10:17:40 +0800

    Update main.m

--- main.m
+++ main.m
@@ -5,11 +5,15 @@ addpath('splsolver')
 % mex  -R2018a sptest5.cpp
 energy_type=7;
 p=1;
-path=sprintf('./SQP/result/%d_%d',energy_type,p);
+list=dir("./tetinterp_meshes");
+path=sprintf('./SQP/obj/%d_%d',energy_type,p);
+if exist(path,'dir')
+    mkdir(path);
+end
+path=sprintf('./SQP/obj/%d_%d',energy_type,p);
 if ~exist(path,'dir')
     mkdir(path);
 end
-list=dir("./tetinterp_meshes");
 d=size(list,1)/2;
 for obj=6:6
     [~,~]=tetmetric(list(2*obj-1).name(1:end-12));
@@ -17,8 +21,6 @@ for obj=6:6
 
     name2=sprintf('./SQP/obj/%s_1.000000.obj',list(2*obj-1).name(1:end-12));
     interpolation(name1,name2,energy_type,p);
-
-
     matname=sprintf("SQP\\result\\%d_%d\\%s_faces.mat",energy_type,p,list(2*obj-1).name(1:end-12));
     save(matname,'faces');
 end
